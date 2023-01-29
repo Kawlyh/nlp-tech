651d6e95a5c4dd8db97f84e3a4f764f1ed985bba
Kawlyh <32896063+Kawlyh@users.noreply.github.com>
2023-01-29 12:24:45 +0800

    Add files via upload

---
+++ my_translators_test.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import translators as ts
+from multiprocessing import Pool
+from tqdm import *
+
+CSV_PATH = 'new_train.csv'
+LANG = 'auto'
+API = 'baidu'
+
+
+def translator_constructor(api):
+    if api == 'google':
+        return ts.google
+    elif api == 'bing':
+        return ts.bing
+    elif api == 'baidu':
+        return ts.baidu
+    elif api == 'sogou':
+        return ts.sogou
+    elif api == 'youdao':
+        return ts.youdao
+    elif api == 'tencent':
+        return ts.tencent
+    elif api == 'alibaba':
+        return ts.alibaba
+    else:
+        raise NotImplementedError(f'{api} translator is not realised!')
+
+
+def translate(x):
+    try:
+        if x[2] == "English":
+            return [x[0], x[1], x[2]]
+        else:
+            return [translator_constructor(API)(x[0], from_language=LANG, to='en'), x[1], x[2]]
+    except:
+        print("failure ")
+        return [None, x[1], x[2]]
+
+
+def imap_unordered_bar(func, args, n_processes: int = 48):
+    p = Pool(n_processes, maxtasksperchild=100)
+    res_list = []
+    with tqdm(total=len(args)) as pbar:
+        for i, res in tqdm(enumerate(p.imap_unordered(func, args))):
+            pbar.update()
+            res_list.append(res)
+    pbar.close()
+    p.close()
+    p.join()
+    return res_list
+
+
+def main():
+    df = pd.read_csv(CSV_PATH)
+    tqdm.pandas()
+    df[['text', 'label', 'language']] = imap_unordered_bar(translate, df[['text', 'label', 'language']].values)
+    df.to_csv(f'English-new-train-{API}.csv', index=False)
+
+
+if __name__ == '__main__':
+    main()
